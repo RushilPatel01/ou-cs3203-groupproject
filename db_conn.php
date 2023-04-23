61b7acbee3860791d2f1a02b219abfdcc9ca3f70
Andrew Graham <80505052+Andrewgrahm1@users.noreply.github.com>
2023-04-23 18:28:26 -0500

    Add files via upload
    successful connection

--- db_conn.php
+++ db_conn.php
@@ -3,8 +3,11 @@ $dbname = "sys";
 $user = "root";
 $pass = "password";
 $port = 3306;
-$conn = mysqli_connect("127.0.0.1", $user, $pass, $dbname);
+$conn = mysqli_connect("172.17.0.1", $user, $pass, $dbname, $port);
 if(!$conn){
     echo "Connection Failed";
+}
+else{
+    echo "Connection worked";
 }
 ?>
