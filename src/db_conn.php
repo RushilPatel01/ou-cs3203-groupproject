5a82877c06e1de3e84f78df0a0c8c845d6afb9e2
Andrew Graham <80505052+Andrewgrahm1@users.noreply.github.com>
2023-04-23 22:48:11 -0500

    Add files via upload

---
+++ src/db_conn.php
@@ -0,0 +1,10 @@
+<?php
+$dbname = "sys";
+$user = "root";
+$pass = "password";
+$port = 3306;
+$conn = mysqli_connect("172.17.0.1", $user, $pass, $dbname, $port);
+if(!$conn){
+    echo "Connection Failed\n";
+}
+?>
