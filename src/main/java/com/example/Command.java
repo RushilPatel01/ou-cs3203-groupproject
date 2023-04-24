e733ccaac4fad9526f85a885892226088ac87885
Andrew Graham <80505052+Andrewgrahm1@users.noreply.github.com>
2023-04-24 16:50:36 -0500

    Delete src/main directory

--- src/main/java/com/example/Command.java
+++
@@ -1,5 +0,0 @@
-package com.example;
-
-public enum Command {
-		LOGIN, CHECKB,UPDATEB, ACCOUNTDISP ;
-}
--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.graphics;
-    requires java.sql;
-
-    opens com.example to javafx.fxml;
-    exports com.example;
-}
