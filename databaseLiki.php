390d746bff23145e548c6d067fc7e873661a4e86
Andrew Graham <80505052+Andrewgrahm1@users.noreply.github.com>
2023-04-24 16:49:04 -0500

    Delete databaseLiki.php

--- databaseLiki.php
+++
@@ -1,8 +0,0 @@
-<?php
-$dbname = "sys";
-$user = "root";
-$pass = "password";
-$port = 3306;
-$conn = mysqli_connect("localhost", $user, $pass, $dbname, $port)
-    or die("Could not connect: " . mysqli_error($con));
-?>
