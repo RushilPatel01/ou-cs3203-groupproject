b35812dc80ccdec2ff5a79d7fd6a29d4e386dafa
Andrew Graham <80505052+Andrewgrahm1@users.noreply.github.com>
2023-04-25 16:05:48 -0500

    Add files via upload

---
+++ ou-cs3203-groupproject-springbootfrontend/MyFirstAzureWebApp/wwwroot/login_page.js
@@ -0,0 +1,13 @@
+window.onload=function(){
+    let redirect_login = document.getElementById('redirect_login');
+
+    redirect_login.addEventListener('click', function(){
+        // Pass through the login page
+        // window.location.href = "dashboard_page_yOUr_Account_Checking.cshtml";
+        window.location.href = "/dashboard_page_yOUr_Account_Checking"
+        
+    }
+
+)};
+
+
